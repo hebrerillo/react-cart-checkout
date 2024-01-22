e16554ec1b783b7c74229a382fc19ffcdfdf974f
Jesus Romero <hebrerillo@gmail.com>
2024-01-22 12:26:09 +0100

    Stable version

--- src/panels/Posts.js
+++ src/panels/Posts.js
@@ -4,10 +4,8 @@ import Post from '../components/Post';
 function Posts() {
 
     const [posts, setPosts] = useState([]);
-    const parentPosts = useRef();
-    const [counterRequests, setCounterRequests] = useState(0);
-    const [counterVisible, setCounterVisible] = useState(0);
     const lastPost = useRef();
+    const parentPosts = useRef();
     const observer = new IntersectionObserver(intersectionCallback, {
         root: null,
         rootMargin: "0px",
@@ -16,40 +14,34 @@ function Posts() {
 
     function intersectionCallback(entries) {
         if (entries[0].target === lastPost.current && entries[0].isIntersecting) {
-            setCounterVisible(counterVisible + 1);
+            fetchData();
         }
     }
 
     async function fetchData() {
-        console.log("getting posts", parentPosts);
         const result = await fetch('http://localhost/posts.php?start=' + parentPosts.current.childNodes.length);
         const remotePosts = await result.json();
-        setPosts((currentPosts) => {
-            return currentPosts.concat(remotePosts);
-        });
+        const newPosts = posts.concat(remotePosts);
+        setPosts(newPosts);
     }
 
     useEffect(() => {
-        fetchData().then(() => {
-            setCounterRequests(() => {
-                return counterRequests + 1;
-            });
-        });
-    }, [counterVisible]);
+        fetchData();
+    }, []);
 
     useEffect(() => {
         if (lastPost.current) {
             observer.observe(lastPost.current);
         }
-    }, [counterRequests]);
+    });
 
     const renderedPosts = posts.map((post, index) => {
         let component = null;
         if (index === posts.length - 1) {
-            component = <div key={post.id} ref={lastPost}><Post post={post} /></div>;
+            component = <div key={post.id} data-key={post.id} ref={lastPost}><Post post={post} /></div>;
         }
         else {
-            component = <div key={post.id} ><Post post={post} /></div>;
+            component = <div key={post.id} data-key={post.id} ><Post post={post} /></div>;
         }
 
         return component;
