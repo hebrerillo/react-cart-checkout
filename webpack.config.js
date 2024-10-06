ffcb776686693e3d3ffc2a7cda9af467ab3ae1db
Jesus Romero <hebrerillo@gmail.com>
2024-10-06 12:05:36 +0200

    Adding Sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,17 @@ module.exports = {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
         use: 'ts-loader'
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader"
+        ]
       }
     ]
   },
