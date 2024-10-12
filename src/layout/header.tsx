3d695a9b0666d317a3d739e7323c4a1715018793
Jesus Romero <hebrerillo@gmail.com>
2024-10-12 11:33:02 +0200

    Left arrow

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 function Header() {
-  return <header>header</header>;
+  return <header className="site-header site-horizontal-padding">
+    <span className="site-header__left-arrow">&larr;</span>
+  </header>;
 }
 
 export default Header;
