7d994ad0cf6de6dede9b388e9de573f0435c86d7
Jesus Romero <hebrerillo@gmail.com>
2024-10-12 12:15:26 +0200

    logo

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 function Header() {
-  return <header className="site-header site-horizontal-padding">
-    <span className="site-header__left-arrow">&larr;</span>
-  </header>;
+  return (
+    <header className="site-header site-horizontal-padding">
+      <span className="site-header__left-arrow">&larr;</span>
+      <a className="site-header__logo" href="#">
+        <img src="/img/logo.svg" />
+      </a>
+    </header>
+  );
 }
 
 export default Header;
