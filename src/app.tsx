a3c802418752e9e553ba8379efdc61687d76976a
Jesus Romero <hebrerillo@gmail.com>
2024-11-24 16:56:58 +0100

    Pretty file

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "src/layout/header";
+import Main from "src/layout/main";
+
+function Checkout() {
+  return (
+    <div className="main-container">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default Checkout;
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -4,10 +4,7 @@ import { useGlobalContext } from "src/context/global";
 function Header() {
   const { siteHeader } = useGlobalContext();
   return (
-    <header
-      className="site-header site-horizontal-padding"
-      ref={siteHeader}
-    >
+    <header className="site-header site-horizontal-padding" ref={siteHeader}>
       <span className="site-header__left-arrow">&larr;</span>
       <a className="site-header__logo" href="#">
         <img src="/img/logo.svg" />
