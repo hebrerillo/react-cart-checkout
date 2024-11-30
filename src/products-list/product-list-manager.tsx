b3aa509b3147e1266448f50e8263fe0758374477
Jesus Romero <hebrerillo@gmail.com>
2024-11-30 10:47:51 +0100

    Remove static methods and use class member methods

--- src/products-list/product-list-manager.tsx
+++ src/products-list/product-list-manager.tsx
@@ -6,7 +6,7 @@ export class ProductListManager {
   /**
    * Performs an asynchronous request to fetch products
    */
-  public static async fetchProducts() {
+  public async fetchProducts() {
     let result = [] as Array<Product>;
 
     const successFunc = (resultRequest: Array<Product>) => {
@@ -25,16 +25,30 @@ export class ProductListManager {
   /**
    * Render the list of products
    */
-  public static renderList(productList: Array<Product>): React.JSX.Element[] {
-    return productList.map((product) => {
-      return (
-        <li className="product__item" key={product.id}>
-          <img className="product__item-img" src={product.img_url} />
-          <div className="product__description">
-            <h3>{product.name}</h3>
-          </div>
-        </li>
-      );
+  public renderList(
+    productList: Array<Product>,
+    lastProduct: React.RefObject<HTMLLIElement>,
+  ): React.JSX.Element[] {
+    return productList.map((product, index) => {
+      if (index === productList.length - 1) {
+        return (
+          <li className="product__item" key={product.id} ref={lastProduct}>
+            <img className="product__item-img" src={product.img_url} />
+            <div className="product__description">
+              <h3>{product.name}</h3>
+            </div>
+          </li>
+        );
+      } else {
+        return (
+          <li className="product__item" key={product.id}>
+            <img className="product__item-img" src={product.img_url} />
+            <div className="product__description">
+              <h3>{product.name}</h3>
+            </div>
+          </li>
+        );
+      }
     });
   }
 }
