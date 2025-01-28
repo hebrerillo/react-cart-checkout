f17f1190d0b56df94fed4f75b4816639b5243d7a
Jesus Romero <hebrerillo@gmail.com>
2025-01-28 09:48:24 +0100

    picture cart

--- src/layout/cart/ProductList.tsx
+++ src/layout/cart/ProductList.tsx
@@ -9,6 +9,9 @@ function ProductList(props: ProductListLayoutProps) {
   const productList = props.cartManager.getList().map((product) => {
     return (
       <li className="cart-modal__item-product" key={product.id}>
+        <picture className="cart-modal__item-picture">
+          <img src={product.desktop_url} />
+        </picture>
         {product.name} x{product.amount}
         <div className="cart-modal__amount-control-wrapper">
           <span
