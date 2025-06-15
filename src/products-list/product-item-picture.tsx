8cfb570168e11eccf772e12132a4e4e3e13665ed
Jesus Romero <hebrerillo@gmail.com>
2025-06-15 13:50:34 +0200

    patch

--- src/products-list/product-item-picture.tsx
+++ src/products-list/product-item-picture.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Product } from "src/products-list/interface";
+import { Spinner } from "src/icons/spinner";
 
 interface ProductPictureProps {
   product: Product;
@@ -8,11 +9,35 @@ interface ProductPictureProps {
 export function ProductPicture(props: ProductPictureProps) {
   const src = props.product.intersects ? props.product.desktop_url : "";
   const alt = props.product.name;
+  const imgRef = useRef<HTMLImageElement>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const timeoutIdRef = useRef(-1);
+
+  useEffect(() => {
+    const imageElement = imgRef?.current;
+    imageElement?.addEventListener("load", (event) => {
+      console.log("image loaded", event, imageElement?.complete);
+      window.clearTimeout(timeoutIdRef.current);
+      setIsLoaded(true);
+    });
+  }, []);
+
+  if (!isLoaded) {
+    timeoutIdRef.current = window.setTimeout(() => {
+      
+    }, 2000);
+  }
 
   return (
     <div className="product__item-picture-container">
+      <Spinner />
       <picture className="product__item-picture">
-        <img data-src={props.product.desktop_url} src={src} alt={alt} />
+        <img
+          data-src={props.product.desktop_url}
+          src={src}
+          alt={alt}
+          ref={imgRef}
+        />
       </picture>
     </div>
   );
