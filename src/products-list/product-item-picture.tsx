4281a1b580f2d8160e21962cdd99f87b5fe3c19f
Jesus Romero <hebrerillo@gmail.com>
2025-06-15 12:48:34 +0200

    Product image in a separate component

--- src/products-list/product-item-picture.tsx
+++ src/products-list/product-item-picture.tsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Product } from "src/products-list/interface";
 
-export function ProductPicture() {
-  return <picture></picture>;
+interface ProductPictureProps {
+  product: Product;
+}
+
+export function ProductPicture(props: ProductPictureProps) {
+  const src = props.product.intersects ? props.product.desktop_url : "";
+  return (
+    <picture>
+      <img
+        className="product__item-img"
+        data-src={props.product.desktop_url}
+        src={src}
+      />
+    </picture>
+  );
 }
--- src/products-list/product-item.tsx
+++ src/products-list/product-item.tsx
@@ -9,7 +9,6 @@ export interface ProductItemProps {
 }
 
 export function ProductItem(props: ProductItemProps) {
-  const src = props.product.intersects ? props.product.desktop_url : "";
   const { cartContextManager } = useCartContext();
 
   const addProductToCart = cartContextManager.addProduct.bind(
@@ -23,12 +22,7 @@ export function ProductItem(props: ProductItemProps) {
       ref={props.refCallback}
       data-id={props.product.id}
     >
-      <ProductPicture />
-      <img
-        className="product__item-img"
-        data-src={props.product.desktop_url}
-        src={src}
-      />
+      <ProductPicture product={props.product} />
       <div className="product__description">
         <h3>{props.product.name}</h3>
       </div>
