6d3c42c01af43966c0608cb1e9b9538f7acc73a6
Jesus Romero <hebrerillo@gmail.com>
2025-09-14 11:34:04 +0200

    Showing an error icon the image downloading has failed

--- src/products-list/product-item-picture.tsx
+++ src/products-list/product-item-picture.tsx
@@ -1,12 +1,14 @@
 import React, { useRef, useState } from "react";
 import { Product } from "src/products-list/interface";
 import { Spinner } from "src/icons/spinner";
+import { ImgLoadError } from "src/icons/img_load_error";
 
 enum PICTURE_STATE {
-  CREATED,
-  LOADING,
-  SLOW_LOADING /* Image is taking too much to load*/,
-  LOADED,
+  CREATED = "created",
+  LOADING = "loading",
+  SLOW_LOADING = "slow_loading" /* Image is taking too much to load, let's show the spinner. */,
+  LOADED = "loaded",
+  ERROR = "error",
 }
 
 interface ProductPictureProps {
@@ -33,39 +35,62 @@ export function ProductPicture(props: ProductPictureProps) {
   const [pictureState, setPictureState] = useState<PICTURE_STATE>(
     PICTURE_STATE.CREATED,
   );
-  const imgRef = useRef<HTMLImageElement>(null);
+
   const timeoutSpinnerId = useRef<number>(-1);
   const src = useRef<string>("");
 
   const intersects = props.product.intersects;
   const alt = props.product.name;
+  let stateImgJSX = <></>;
 
-  let spinnerJSX =
-    pictureState === PICTURE_STATE.SLOW_LOADING ? <Spinner /> : <></>;
+  /**
+   * Callback executed when image has finished loading. The callback covers both
+   * the success and fail cases.
+   *
+   * @param success - true if the image has been loaded successfully, false otherwise.
+   */
+  const onProductImageLoad = (success: boolean, _: React.SyntheticEvent) => {
+    if (pictureState === PICTURE_STATE.CREATED) {
+      return;
+    }
 
-  if (intersects && pictureState === PICTURE_STATE.CREATED) {
-    src.current = props.product.desktop_url;
-    setPictureState(PICTURE_STATE.LOADING);
+    const state = success ? PICTURE_STATE.LOADED : PICTURE_STATE.ERROR;
+    clearSpinnerTimeout(timeoutSpinnerId);
+    setPictureState(state);
+  };
 
-    imgRef.current?.addEventListener("load", () => {
-      clearSpinnerTimeout(timeoutSpinnerId);
-      setPictureState(PICTURE_STATE.LOADED);
-    });
+  /**
+   * Handles the state and initial variables before rendering the component.
+   */
+  const beforeRender = () => {
+    if (pictureState === PICTURE_STATE.SLOW_LOADING) {
+      stateImgJSX = <Spinner />;
+    } else if (pictureState === PICTURE_STATE.ERROR) {
+      stateImgJSX = <ImgLoadError />;
+    }
 
-    timeoutSpinnerId.current = window.setTimeout(() => {
-      setPictureState(PICTURE_STATE.SLOW_LOADING);
-    }, SPINNER_TIMEOUT);
-  }
+    if (intersects && pictureState === PICTURE_STATE.CREATED) {
+      src.current = props.product.desktop_url;
+      setPictureState(PICTURE_STATE.LOADING);
+
+      timeoutSpinnerId.current = window.setTimeout(() => {
+        setPictureState(PICTURE_STATE.SLOW_LOADING);
+      }, SPINNER_TIMEOUT);
+    }
+  };
+
+  beforeRender();
 
   return (
     <div className="product__item-picture-container">
-      {spinnerJSX}
+      {stateImgJSX}
       <picture className="product__item-picture">
         <img
           data-src={props.product.desktop_url}
           src={src.current}
           alt={alt}
-          ref={imgRef}
+          onLoad={onProductImageLoad.bind(null, true)}
+          onError={onProductImageLoad.bind(null, false)}
         />
       </picture>
     </div>
